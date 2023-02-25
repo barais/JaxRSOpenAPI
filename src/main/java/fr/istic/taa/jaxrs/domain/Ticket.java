84962698c181f2c01cc1c8f55b350c41bec0052e
abrou <assamela.brou@etudiant.univ-rennes1.fr>
2023-02-25 13:21:49 +0100

    Association Ticket-User/Membresup

--- src/main/java/fr/istic/taa/jaxrs/domain/Ticket.java
+++ src/main/java/fr/istic/taa/jaxrs/domain/Ticket.java
@@ -2,10 +2,15 @@ package fr.istic.taa.jaxrs.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Ticket {
     private long id;
+    private User auteur;
+    private Membresup membresup;
+
     @Id
     public long getId() {
         return id;
@@ -13,5 +18,23 @@ public class Ticket {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @ManyToOne
+    public User getAuteur() {
+        return this.auteur;
+    }
+
+    public void setAuteur(User auteur) {
+        this.auteur = auteur;
+    }
+
+    @ManyToOne
+    public Membresup getMembresup() {
+        return this.membresup;
+    }
+
+    public void setMembresup(Membresup membresup) {
+        this.membresup = membresup;
     }
 }
